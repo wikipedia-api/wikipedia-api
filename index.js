c40fc88a5fcfbbce63396a80fdc63bc4327941fd
sennrdesign <sennadeturner@gmail.com>
2019-09-03 15:16:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.getElementById('search').oninput = function(){
   let http = new XMLHttpRequest()
   let url = 'https://nl.wikipedia.org/w/api.php?format=json&action=opensearch&limit=5&search=' + this.value
   http.open('get', url)
-  http.setRequestHeader('Access-Control-Allow-Headers', '*');
+  http.setRequestHeader('Access-Control-Allow-Headers', 'https://wikipedia-api.github.io');
   http.send()
   http.onreadystatechange = (e) => {
     console.log(http.responseText)
