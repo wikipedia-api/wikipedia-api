a9761943b62d76f83d7bf46fe25fc3d51f397919
sennrdesign <sennadeturner@gmail.com>
2019-09-03 15:00:11 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('search').oninput = function(){
+  let http = new XMLHttpRequest()
+  let url = 'nl.wikipedia.org/w/api.php?format=json&action=opensearch&limit=5&search=' + this.value
+  http.open('get', url)
+  http.send()
+  http.onreadystatechange = (e) => {
+    console.log(http.responseText)
+  }
+}
