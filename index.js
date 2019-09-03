c19009d2fa45a86d64af6f18bcb0b89a6826e713
sennrdesign <sennadeturner@gmail.com>
2019-09-03 15:04:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.getElementById('search').oninput = function(){
   let http = new XMLHttpRequest()
-  let url = 'nl.wikipedia.org/w/api.php?format=json&action=opensearch&limit=5&search=' + this.value
+  let url = 'https://nl.wikipedia.org/w/api.php?format=json&action=opensearch&limit=5&search=' + this.value
   http.open('get', url)
   http.send()
   http.onreadystatechange = (e) => {
